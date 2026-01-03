1438995ceadbae03d6e5e59061d95847522ffdbe
siwarjawabreh2003 <siwar.jawabreh2003@gmail.com>
2026-01-03 13:37:53 +0300

    Add files via upload

---
+++ lab1.dart
@@ -0,0 +1,38 @@
+ //import 'dart:io';
+ void main (){
+
+
+  //print("full name is $firstName $lastName ");// $ لحتى تتعامل في الstring انها متغير وتنطبق قيمتها المخزنه فيها 
+// بنقدر نستخدم ال + كمان بس انها لازم تجمع شيئين نفس بعض لهيك $ افضل 
+//List<String> persons =["siwar","noor","sami"];
+//print(" my name is ${persons[1]}");
+//} 
+/* الكلاس فيها ميثود وفنكشن
+Map<String,dynamic>person ={
+  "name":"ahmad",
+  "age":22,
+  "hobby":["swim","read"],
+  "address":{"streeName":"sss"},*/
+/*print("enter");
+var num =  int.parse(so.readLineSync();
+print("the name ${name}");
+print(num.runtimeType);*/
+
+/*String name = "Siwar";
+print("Hello $name");
+var pi =3.14;
+pi= 332.5;
+ print(" value$pi");
+ double price=1130.256585; 
+ print(price.toStringAsFixed(2));
+ String namess="""
+gujkh
+ojljhhj
+lkpk""";
+print(namess);*/
+/*var str ="jordan, oman, dobi";
+print(str.split(","));*/
+
+
+
+ }
